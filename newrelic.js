b1932662cfd0ec1d751c0e6ae34f983e026d0f3b
BlueHotDog <dannifriedland@gmail.com>
2014-10-27 18:56:59 +0200

    adding newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['iwantgif'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '16bd11ebe3b08da23fe0092db00bc7f221dc3b05',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
